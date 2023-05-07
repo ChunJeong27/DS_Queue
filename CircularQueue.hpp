edc6189bf382dc1f786db2fffa40d81ae9b3c25e
ChunJeong <hun4742@gmail.com>
2023-05-07 16:33:42 +0900

    원형 큐 헤더파일 추가

--- CircularQueue.hpp
+++ CircularQueue.hpp
@@ -3,4 +3,27 @@
 
 #include <iostream>
 
+using Data = int;
+
+class Queue
+{
+public:
+    Queue();
+
+    void init();
+    void isEmpty();
+
+    void enqueue(Data data);
+    Data dequeue();
+    Data peek();
+
+private:
+    static int const QUE_LEN = 100;
+
+    int front;
+    int rear;
+    Data queArr[QUE_LEN];
+
+};
+
 #endif  // __CIRCULAR_QUEUE_HPP__
