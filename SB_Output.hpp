cd3387962facd5ad448d0120ca52b3c96f28a38b
ChunJeong <hun4742@gmail.com>
2023-05-14 18:45:11 +0900

    서브모듈 테스트용 출력 헤더 작성

---
+++ SB_Output.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef __SB_OUTPUT_HPP__
+#define __SB_OUTPUT_HPP__
+
+#include <iostream>
+
+class SB_Output
+{
+    void print()
+    {
+        std::cout << "Version 1.0" << std::endl;
+    }
+
+};
+
+
+#endif // __SB_OUTPUT_HPP__
